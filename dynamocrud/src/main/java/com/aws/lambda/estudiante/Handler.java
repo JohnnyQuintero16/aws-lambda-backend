230fa303ef3e716f9101e3de68125c826410ff0f
JohnnyQuintero16 <johnnyaquintero@gmail.com>
2022-10-22 18:00:10 -0500

    handler estudiante

--- dynamocrud/src/main/java/com/aws/lambda/estudiante/Handler.java
+++ dynamocrud/src/main/java/com/aws/lambda/estudiante/Handler.java
@@ -1,38 +1,46 @@
 package com.aws.lambda.estudiante;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Handler implements RequestHandler<Request, Object> {
 
     @Override
     public Object handleRequest(Request request, Context context) {
 
-       /* AmazonDynamoDB db = AmazonDynamoDBClientBuilder.defaultClient();
+        AmazonDynamoDB db = AmazonDynamoDBClientBuilder.defaultClient();
         DynamoDBMapper mapper = new DynamoDBMapper(db);
-        Producto producto = null;
+        Estudiante estudiante = null;
 
        switch (request.getHttpMethod()) {
             case "GET":
                 if (request.getId() == 0) {
-                    List<Producto> productos = new ArrayList<>();
-                    productos = mapper.scan(Producto.class, new DynamoDBScanExpression());
-                    return productos;
+                    List<Estudiante> estudiantes = new ArrayList<>();
+                    estudiantes = mapper.scan(Estudiante.class, new DynamoDBScanExpression());
+                    return estudiantes;
                 } else {
-                    producto = mapper.load(Producto.class, request.getId());
-                    return producto;
+                    estudiante = mapper.load(Estudiante.class, request.getId());
+                    return estudiante;
                 }
 
-            case "POST":
-                producto = request.getProducto();
-                mapper.save(producto);
-                return producto;
+            case "PUT":
+                estudiante = request.getEstudiante();
+                mapper.save(estudiante);
+                return estudiante;
 
             case "DELETE":
-                producto = mapper.load(Producto.class, request.getId());
-                if (producto != null)
-                    mapper.delete(producto);
-                return producto;
-        }*/
+                estudiante = mapper.load(Estudiante.class, request.getId());
+                if (estudiante != null)
+                    mapper.delete(estudiante);
+                return estudiante;
+        }
         return null;
     }
 
