0c8b26f8ad9dd679d93f11b547b5b717a58ade7e
SamanthaZamora <samanthaelianza@ufps.edu.co>
2022-10-23 16:57:05 -0500

    Commit de Johnny

--- dynamocrud/src/main/java/com/aws/lambda/estudiante/Handler.java
+++ dynamocrud/src/main/java/com/aws/lambda/estudiante/Handler.java
@@ -19,12 +19,21 @@ public class Handler implements RequestHandler<Request, Object> {
         DynamoDBMapper mapper = new DynamoDBMapper(db);
         Estudiante estudiante = null;
 
-       switch (request.getHttpMethod()) {
+        switch (request.getHttpMethod()) {
             case "GET":
                 return mapper.scan(Estudiante.class, new DynamoDBScanExpression())
                         .stream()
                         .filter( estu -> estu.getIdMateria() == request.getId());
-                
+
+                /*if (request.getId() == 0) {
+                    List<Estudiante> estudiantes = new ArrayList<>();
+                    estudiantes = mapper.scan(Estudiante.class, new DynamoDBScanExpression());
+                    return estudiantes;
+                } else {
+                    estudiante = mapper.load(Estudiante.class, request.getId());
+                    return estudiante;
+                }*/
+
             case "POST":
                 estudiante = request.getEstudiante();
                 mapper.save(estudiante);
